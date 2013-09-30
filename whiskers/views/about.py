16bf0b5a08343fb066d0fe76bb5ae9bbf9180d0d
Jukka Ojaniemi <jukka.ojaniemi@gmail.com>
2013-09-30 21:48:14 +0300

    Removed restructured files.

--- whiskers/views/about.py
+++
@@ -1,6 +0,0 @@
-from pyramid.renderers import get_renderer
-
-
-def about_view(request):
-    main = get_renderer('whiskers:views/templates/master.pt').implementation()
-    return {'project': 'whiskers', 'main': main}
