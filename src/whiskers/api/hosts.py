22b2e602086d8b741ea92667e349e855b40034f7
Jukka Ojaniemi <jukka.ojaniemi@gmail.com>
2013-10-02 23:10:53 +0300

    Added tests for hosts rest api.

--- src/whiskers/api/hosts.py
+++ src/whiskers/api/hosts.py
@@ -21,6 +21,15 @@ class HostsAPI(object):
         return {'hosts': results}
 
     @view(renderer='json')
+    def collection_post(self):
+        """Stores host to database."""
+        data = self.request.json_body
+        host = Host(**data)
+        DBSession.add(host)
+        DBSession.flush()
+        return host.get_as_dict()
+
+    @view(renderer='json')
     def get(self):
         host_id = self.request.matchdict.get('host_id')
         host = Host.get_by_id(host_id)
---
+++ src/whiskers/tests/test_rest_hosts.py
@@ -0,0 +1,44 @@
+import unittest
+
+from pyramid.paster import get_app
+from whiskers.models import DBSession, Host
+from webtest import TestApp
+
+
+class RESTHostsTest(unittest.TestCase):
+
+    def setUp(self):
+        app = get_app('testing.ini')
+        self.test_app = TestApp(app)
+
+    def add_host(self):
+        host = Host('test_host_1', '1.1.1.1')
+        DBSession.add(host)
+        DBSession.flush()
+
+    def common_tests(self, resp):
+        self.assertEqual(resp.content_type, 'application/json')
+        self.assertEqual(resp.status, '200 OK')
+
+    def test_hosts(self):
+        resp = self.test_app.get('/api/hosts')
+        self.common_tests(resp)
+
+    def test_host_put(self):
+        host_data = {'name': 'test_host_2',
+                     'ipv4': '2.2.2.2'}
+        resp = self.test_app.post_json('/api/hosts', host_data)
+        self.common_tests(resp)
+        self.assertEqual(resp.json[u'name'], u'test_host_2')
+        self.assertEqual(resp.json[u'ipv4'], u'2.2.2.2')
+
+    def test_host_get(self):
+        host_data = {'name': 'test_host_3',
+                     'ipv4': '3.3.3.3'}
+        self.test_app.post_json('/api/hosts', host_data)
+        resp = self.test_app.get('/api/hosts/1')
+        self.common_tests(resp)
+
+
+if __name__ == '__main__':
+    unittest.main()
