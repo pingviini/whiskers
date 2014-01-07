282e5a6027e5bcd5ed526c16869a18d1d1561d5e
Jukka Ojaniemi <jukka.ojaniemi+github@live.com>
2014-01-07 11:05:04 +0200

    Returns the amount of unique buildouts / host. Fixes #9.

--- src/whiskers/views/hosts.py
+++ src/whiskers/views/hosts.py
@@ -1,5 +1,5 @@
-from sqlalchemy.sql.expression import func
 from pyramid.renderers import get_renderer
+from sqlalchemy.orm.exc import NoResultFound
 from whiskers.models import DBSession
 from whiskers.models import (
     Host,
@@ -16,18 +16,23 @@ class HostsView(object):
 
     def __call__(self):
         """Hosts main view."""
-        query = self.get_hosts_info()
+        query = self.hosts_info
         return {'results': query, 'main': self.main}
 
     def host_view(self):
         host_id = self.request.matchdict['host_id']
-        host = Host.get_by_id(int(host_id))
-        buildouts = self.get_buildouts(host_id)
+        try:
+            host = Host.get_by_id(int(host_id))
+            buildouts = self.get_buildouts(host_id)
+        except NoResultFound as e:
+            host = None
+            buildouts = []
         return {'host': host,
                 'main': self.main,
                 'buildouts': buildouts}
 
-    def get_hosts_info(self):
+    @property
+    def hosts_info(self):
         """Return list of dicts containing Host info."""
 
         result_list = []
@@ -36,12 +41,19 @@ class HostsView(object):
             join(Buildout, Buildout.host_id == Host.id).all()
 
         for result in results:
-            tmp = {}
-            tmp['host'] = result
-            tmp['count'] = len(result.buildouts)
+            tmp = {'host': result, 'count': self.get_unique_buildouts(result)}
             result_list.append(tmp)
 
         return result_list
+
+    def get_unique_buildouts(self, host):
+        """Return amount of buildouts (grouped by name)."""
+
+        results = DBSession.query(Buildout).\
+            filter(Buildout.host == host).\
+            group_by(Buildout.name)
+
+        return results.count()
 
     def get_buildouts(self, host_id):
         """Return list of buildouts."""
